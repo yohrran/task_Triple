83eed7eaebb99d3f0203127d46928c2f8485aa9e
yohrran <johnyohan93@gmail.com>
2022-06-18 18:28:54 +0900

    fix: react-router-dom setting

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import RootPage from './page/RootPage/RootPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<RootPage />} />
+    </Routes>
+  </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/page/RootPage/RootPage.js
@@ -0,0 +1,9 @@
+function RootPage() {
+  return (
+    <div>
+      <p>rootPage</p>
+    </div>
+  );
+}
+
+export default RootPage;
