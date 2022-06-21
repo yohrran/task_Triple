f4bd43376825f6465c6d9ab0c847899a34d9b1dd
yohrran <johnyohan93@gmail.com>
2022-06-21 20:06:46 +0900

    "feat: Index add NotFound and delete comment"

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import MainPage from './page/MainPage/MainPage';
+import NotFound from './page/NotFound/NotFound';
 import GlobalStyle from './utils/GlobalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,6 +12,7 @@ root.render(
     <GlobalStyle />
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>,
 );
