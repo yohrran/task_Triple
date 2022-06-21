7982a2a11cf59fc9e630a6082acc62166c1ce1f7
yohrran <johnyohan93@gmail.com>
2022-06-21 17:12:14 +0900

    "fix: eslint modify"

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
   rules: {
     'prettier/prettier': ['error', { singleQuote: true, parser: 'flow' }],
     'no-console': 'error',
+    'no-unused-expressions': 'off',
   },
 };
